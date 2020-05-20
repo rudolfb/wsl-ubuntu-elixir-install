757aa3a1196ccbb6f25eb2dd65db7c403aa8e7ef
Rudolf Bargholz <rudolf@bargholz.ch>
2020-05-20 08:34:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ sudo apt -y install \
 
 sudo apt -y update
 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 sudo apt -y install nodejs
 
