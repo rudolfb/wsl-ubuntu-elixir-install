233e33f02e6f68c13e5d185fd5e5def15f2a04d6
Rudolf Bargholz <rudolf@bargholz.ch>
2020-07-20 15:54:28 +0200

    Update elixir-erlang-asdf-install.sh

--- elixir-erlang-asdf-install.sh
+++ elixir-erlang-asdf-install.sh
@@ -1,8 +1,8 @@
 asdf install erlang 23.0.2
 asdf global erlang 23.0.2
 
-asdf install elixir 1.10.4-otp-22
-asdf global elixir 1.10.4-otp-22
+asdf install elixir 1.10.4-otp-23
+asdf global elixir 1.10.4-otp-23
 
 asdf install rust 1.44.1
 asdf global rust 1.44.1
