c2bd5fbf77e14c217138c68e7adb47bf5868a8c5
Rudolf Bargholz <rudolf@bargholz.ch>
2020-07-23 04:26:33 +0200

    Update phoenix-install.sh

--- phoenix-install.sh
+++ phoenix-install.sh
@@ -1,5 +1,5 @@
 mix do local.hex --force , \
   local.rebar --force , \
-  archive.install hex phx_new 1.5.3 --force
+  archive.install hex phx_new 1.5.4 --force
 mix do local.phx --force
 mix hex.info
