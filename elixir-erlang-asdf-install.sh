4e30d364259f1554d655fd9d818694b6b6b07c5b
Rudolf Bargholz <rudolf@bargholz.ch>
2020-09-13 22:28:05 +0200

    Update asf plugins
    Added code to update the asdf plugins before installation

--- elixir-erlang-asdf-install.sh
+++ elixir-erlang-asdf-install.sh
@@ -1,12 +1,16 @@
+asdf plugin update erlang
 asdf install erlang 23.0.4
 asdf global erlang 23.0.4
 
+asdf plugin update elixir
 asdf install elixir 1.10.4-otp-23
 asdf global elixir 1.10.4-otp-23
 
+asdf plugin update rust
 asdf install rust 1.46.0
 asdf global rust 1.46.0
 
+asdf plugin update ruby
 asdf install ruby 2.7.1
 asdf global ruby 2.7.1
 gem install htmlbeautifier
