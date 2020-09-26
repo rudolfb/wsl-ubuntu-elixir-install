08a72b029da9dd01262c542213d2189e8395b9a8
Rudolf Bargholz <rudolf@bargholz.ch>
2020-09-26 19:43:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,6 +39,8 @@ sudo apt -y install nodejs
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg-testing main 13" |sudo tee  /etc/apt/sources.list.d/pgdg-testing.list
+
 sudo apt update
 sudo apt -y install postgresql-client-13
 sudo apt -y install postgresql-13
