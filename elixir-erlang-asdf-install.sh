500469f6c4a74653956fd0c9aa83532c05dc0190
Rudolf Bargholz <rudolf@bargholz.ch>
2020-10-04 09:52:45 +0200

    Update Erlang version number
    and use script variables

--- elixir-erlang-asdf-install.sh
+++ elixir-erlang-asdf-install.sh
@@ -1,18 +1,23 @@
+ERLANGVERSION=23.1.1
+ELIXIRVERSION=1.10.4-otp-23
+RUSTVERSION=1.46.0
+RUBYVERSION=2.7.1
+
 asdf plugin update erlang
-asdf install erlang 23.1
-asdf global erlang 23.1
+asdf install erlang $ERLANGVERSION
+asdf global erlang $ERLANGVERSION
 
 asdf plugin update elixir
-asdf install elixir 1.10.4-otp-23
-asdf global elixir 1.10.4-otp-23
+asdf install elixir $ELIXIRVERSION
+asdf global elixir $ELIXIRVERSION
 
 asdf plugin update rust
-asdf install rust 1.46.0
-asdf global rust 1.46.0
+asdf install rust $RUSTVERSION
+asdf global rust $RUSTVERSION
 
 asdf plugin update ruby
-asdf install ruby 2.7.1
-asdf global ruby 2.7.1
+asdf install ruby $RUBYVERSION
+asdf global ruby $RUBYVERSION
 gem install htmlbeautifier
 gem update htmlbeautifier
 
