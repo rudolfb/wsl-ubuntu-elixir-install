247fc9954c3f08d3b7837459493ace76a936e06b
Rudolf Bargholz <rudolf@bargholz.ch>
2020-10-04 10:17:21 +0200

    Changed variables to snake case
    Easier to read

--- elixir-erlang-asdf-install.sh
+++ elixir-erlang-asdf-install.sh
@@ -1,36 +1,36 @@
-LATESTERLANGVERSION=$(asdf latest erlang)
-LATESTELIXIRVERSION=$(asdf latest elixir)
-LATESTRUSTVERSION=$(asdf latest rust)
-LATESTRUBYVERSION=$(asdf latest ruby)
+LATEST_ERLANG_VERSION=$(asdf latest erlang)
+LATEST_ELIXIR_VERSION=$(asdf latest elixir)
+LATEST_RUST_VERSION=$(asdf latest rust)
+LATEST_RUBY_VERSION=$(asdf latest ruby)
 
 set -x
 
-ERLANGVERSION=23.1.1
-echo "Latest Erlang version: ${LATESTERLANGVERSION}"
-ELIXIRVERSION=1.10.4-otp-23
-echo "Latest Elixir version: ${LATESTELIXIRVERSION}"
-RUSTVERSION=1.46.0
-echo "Latest Rust version: ${LATESTRUSTVERSION}"
-RUBYVERSION=2.7.1
-echo "Latest Ruby version: ${LATESTRUBYVERSION}"
+ERLANG_VERSION=23.1.1
+echo "Latest Erlang version: ${LATEST_ERLANG_VERSION}"
+ELIXIR_VERSION=1.10.4-otp-23
+echo "Latest Elixir version: ${LATEST_ELIXIR_VERSION}"
+RUST_VERSION=1.46.0
+echo "Latest Rust version: ${LATEST_RUST_VERSION}"
+RUBY_VERSION=2.7.1
+echo "Latest Ruby version: ${LATEST_RUBY_VERSION}"
 
 asdf plugin update --all
 
 asdf plugin update erlang
-asdf install erlang $ERLANGVERSION
-asdf global erlang $ERLANGVERSION
+asdf install erlang $ERLANG_VERSION
+asdf global erlang $ERLANG_VERSION
 
 asdf plugin update elixir
-asdf install elixir $ELIXIRVERSION
-asdf global elixir $ELIXIRVERSION
+asdf install elixir $ELIXIR_VERSION
+asdf global elixir $ELIXIR_VERSION
 
 asdf plugin update rust
-asdf install rust $RUSTVERSION
-asdf global rust $RUSTVERSION
+asdf install rust $RUST_VERSION
+asdf global rust $RUST_VERSION
 
 asdf plugin update ruby
-asdf install ruby $RUBYVERSION
-asdf global ruby $RUBYVERSION
+asdf install ruby $RUBY_VERSION
+asdf global ruby $RUBY_VERSION
 gem install htmlbeautifier
 gem update htmlbeautifier
 
