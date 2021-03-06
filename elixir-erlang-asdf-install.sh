bc1678eacf75427eed196b217b72609acd971735
Rudolf Bargholz <rudolf@bargholz.ch>
2021-03-06 09:58:14 +0100

    Changed code order
    and updated the script to check if the version numbers in the script need to be updated.

--- elixir-erlang-asdf-install.sh
+++ elixir-erlang-asdf-install.sh
@@ -17,21 +17,29 @@ CURRENT_RUST_VERSION=$(asdf current rust)
 CURRENT_RUBY_VERSION=$(asdf current ruby)
 
 echo ""
-echo "Erlang version to install: ${ERLANG_VERSION}"
-echo "Current Erlang version: ${CURRENT_ERLANG_VERSION}"
 echo "Latest Erlang version: ${LATEST_ERLANG_VERSION}"
+echo "Current Erlang version: ${CURRENT_ERLANG_VERSION}"
+echo "Erlang version to install: ${ERLANG_VERSION}"
+if [[ "${LATEST_ERLANG_VERSION}" != "${ERLANG_VERSION}" ]]
+then
+  echo UPDATE GITHUB: Need to update github and modify the version of Erlang to install
+fi
+
 echo ""
-echo "Elixir version to install: ${ELIXIR_VERSION}"
-echo "Current Elixir version: ${CURRENT_ELIXIR_VERSION}"
 echo "Latest Elixir version: ${LATEST_ELIXIR_VERSION}"
+echo "Current Elixir version: ${CURRENT_ELIXIR_VERSION}"
+echo "Elixir version to install: ${ELIXIR_VERSION}"
+
 echo ""
-echo "Rust version to install: ${RUST_VERSION}"
-echo "Current Rust version: ${CURRENT_RUST_VERSION}"
 echo "Latest Rust version: ${LATEST_RUST_VERSION}"
+echo "Current Rust version: ${CURRENT_RUST_VERSION}"
+echo "Rust version to install: ${RUST_VERSION}"
+
 echo ""
-echo "Ruby version to install: ${RUBY_VERSION}"
-echo "Current Ruby version: ${CURRENT_RUBY_VERSION}"
 echo "Latest Ruby version: ${LATEST_RUBY_VERSION}"
+echo "Current Ruby version: ${CURRENT_RUBY_VERSION}"
+echo "Ruby version to install: ${RUBY_VERSION}"
+
 echo ""
 
 set -x
