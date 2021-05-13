ee66c8b788d597de869b17ed0360617f92e0e8b6
Rudolf Bargholz <rudolf@bargholz.ch>
2021-05-13 09:12:15 +0200

    Bash display is too verbose for current phoenix version

--- phoenix-install.sh
+++ phoenix-install.sh
@@ -5,7 +5,6 @@ mix do local.hex --force , \
   archive.install hex phx_new 1.5.9 --force
 mix do local.phx --force
 mix hex.info
+set +x
 CURRENT_PHOENIX_VERSION=$(mix phx.new -v)
 echo "Current phoenix version: ${CURRENT_PHOENIX_VERSION}"
-  
-set +x
