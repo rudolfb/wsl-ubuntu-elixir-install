b96efc2738c7f131f018c2c9dc951f605c5342e7
Rudolf Bargholz <rudolf@bargholz.ch>
2021-05-13 10:47:25 +0200

    Fetch latest phoenix version
    This allows me to visibly see if the phoenix version being installed by the script is actually the latest available version.

--- phoenix-install.sh
+++ phoenix-install.sh
@@ -7,4 +7,6 @@ mix do local.phx --force
 mix hex.info
 set +x
 CURRENT_PHOENIX_VERSION=$(mix phx.new -v)
+LATEST_PHOENIX_VERSION=$(curl -s "https://api.github.com/repos/phoenixframework/phoenix/tags" | jq -r '.[0].name')
 echo "Current phoenix version: ${CURRENT_PHOENIX_VERSION}"
+echo "Latest phoenix version: ${LATEST_PHOENIX_VERSION}"
