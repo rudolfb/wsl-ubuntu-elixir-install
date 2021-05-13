3924a75b0954b0a2e625c7890daddd6f9f8ae719
Rudolf Bargholz <rudolf@bargholz.ch>
2021-05-13 12:03:49 +0200

    Reformat display
    Current and latest version numbers left aligned

--- phoenix-install.sh
+++ phoenix-install.sh
@@ -9,4 +9,4 @@ set +x
 CURRENT_PHOENIX_VERSION=$(mix phx.new -v)
 LATEST_PHOENIX_VERSION=$(curl -s "https://api.github.com/repos/phoenixframework/phoenix/tags" | jq -r '.[0].name')
 echo "Currently installed phoenix version: ${CURRENT_PHOENIX_VERSION}"
-echo "Latest available phoenix version: ${LATEST_PHOENIX_VERSION}"
+echo "Latest available phoenix version:            ${LATEST_PHOENIX_VERSION}"
