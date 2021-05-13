25da0546c9aa29adf666f91655a3ebbaf224dc52
Rudolf Bargholz <rudolf@bargholz.ch>
2021-05-13 10:48:35 +0200

    Clearer echo description

--- phoenix-install.sh
+++ phoenix-install.sh
@@ -8,5 +8,5 @@ mix hex.info
 set +x
 CURRENT_PHOENIX_VERSION=$(mix phx.new -v)
 LATEST_PHOENIX_VERSION=$(curl -s "https://api.github.com/repos/phoenixframework/phoenix/tags" | jq -r '.[0].name')
-echo "Current phoenix version: ${CURRENT_PHOENIX_VERSION}"
-echo "Latest phoenix version: ${LATEST_PHOENIX_VERSION}"
+echo "Currently installed phoenix version: ${CURRENT_PHOENIX_VERSION}"
+echo "Latest available phoenix version: ${LATEST_PHOENIX_VERSION}"
