18da7de10553dc1179e46970c547861efe9ee8c4
Rudolf Bargholz <rudolf@bargholz.ch>
2021-08-25 08:49:10 +0200

    Update base phoenix version

--- phoenix-install.sh
+++ phoenix-install.sh
@@ -2,7 +2,7 @@ set -x
 
 mix do local.hex --force , \
   local.rebar --force , \
-  archive.install hex phx_new 1.5.9 --force
+  archive.install hex phx_new 1.5.12 --force
 mix do local.phx --force
 mix hex.info
 set +x
