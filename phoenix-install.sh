e4a7b1d16fb7c7e2829af7e5bdeec3eff4a38506
Rudolf Bargholz <rudolf@bargholz.ch>
2022-10-11 08:59:40 +0200

    Change echo logging
    Prevent too verbose logging display

--- phoenix-install.sh
+++ phoenix-install.sh
@@ -1,14 +1,16 @@
-set -x
+set +x
 
 CURRENT_PHOENIX_VERSION=$(mix phx.new -v)
 echo "Installed phoenix version before update: ${CURRENT_PHOENIX_VERSION}"
+
+set -x
 
 mix do local.hex --force , \
   local.rebar --force , \
   archive.install hex phx_new 1.6.14 --force
 mix do local.phx --force
 mix hex.info
-set +x
+
 CURRENT_PHOENIX_VERSION=$(mix phx.new -v)
 LATEST_PHOENIX_VERSION=$(curl -s "https://api.github.com/repos/phoenixframework/phoenix/tags" | jq -r '.[0].name')
 echo "Currently installed phoenix version: ${CURRENT_PHOENIX_VERSION}"
